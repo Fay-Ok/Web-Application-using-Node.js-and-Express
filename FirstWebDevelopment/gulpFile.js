96cdf72bd577b7e7e9fa327e06b39934dc4b1979
Fay-OK <Farzaneh.orak@gmail.com>
2016-06-21 22:42:13 -0700

    Added nodemon and serve gulp task.

--- FirstWebDevelopment/gulpFile.js
+++ FirstWebDevelopment/gulpFile.js
@@ -26,10 +26,26 @@ gulp.task('inject', function () {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
         ignorePath: '../../public'
-    }
+    };
 
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
-})
+});
+
+gulp.task('serve', ['style', 'inject'], function () {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 5000
+        },
+        watch: jsFiles
+    };
+    return nodemon(options)
+        .on('restart',function (ev) {
+            console.log('restarting...');
+            
+        });
+});
