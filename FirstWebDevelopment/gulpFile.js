d8ad3362ce09260fe6c8b8f87dd1bb9a0fb86f35
Fay-OK <Farzaneh.orak@gmail.com>
2016-06-21 22:34:06 -0700

    Added nodemon and managed the js and css injection.

--- FirstWebDevelopment/gulpFile.js
+++ FirstWebDevelopment/gulpFile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var jsFiles = ['*.js', 'src/**/*.js'];
+var nodemon = require('gulp-nodemon');
 
 gulp.task('style', function () {
     return gulp.src(jsFiles)
@@ -14,6 +15,13 @@ gulp.task('style', function () {
 
 gulp.task('inject', function () {
     var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], { read: false });
+
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+
     var options = {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
@@ -22,5 +30,6 @@ gulp.task('inject', function () {
 
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
 })
