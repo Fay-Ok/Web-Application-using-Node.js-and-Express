4b71c98699415074663e44287b6abb50ac8d33b3
Fay-OK <Farzaneh.orak@gmail.com>
2016-06-27 20:51:57 -0700

    added Books and Author into navigation bar.

--- FirstWebDevelopment/app.js
+++ FirstWebDevelopment/app.js
@@ -6,11 +6,17 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.set('views','./src/views');
-app.set('view engine','ejs');
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('index',{list: ['a','b']});
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [
+            { Link: '/Books', Text: 'Books' },
+            { Link: '/Authors', Text: 'Authors' }
+        ]
+    });
 });
 
 app.get('/books', function (req, res) {
