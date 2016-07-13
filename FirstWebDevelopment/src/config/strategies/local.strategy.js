0fd978ecabbc13cafc662c7a57c0aa529f759ee4
Fay-OK <Farzaneh.orak@gmail.com>
2016-07-12 20:16:08 -0700

    added an id for each entry in database.

--- FirstWebDevelopment/src/config/strategies/local.strategy.js
+++ FirstWebDevelopment/src/config/strategies/local.strategy.js
@@ -1,7 +1,6 @@
 var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
-
-
+    LocalStrategy = require('passport-local').Strategy,
+    mongodb = require('mongodb').MongoClient;
 
 module.exports = function () {
     passport.use(new LocalStrategy({
@@ -10,13 +9,15 @@ module.exports = function () {
 
     },
         function (username, password, done) {
-            var user = {
-                username: username,
-                password: password
-            };
-
-            done(null, user);
-
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('users');
+                collection.findOne({
+                    username: username
+                }, function (err, results) {
+                    var user = results;
+                    done(null, user);
+                });
+            });
         }));
-
 };
