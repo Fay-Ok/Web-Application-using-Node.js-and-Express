1b914c72184944393217ec81f7e4400d28570d99
Fay-OK <Farzaneh.orak@gmail.com>
2016-07-18 21:42:56 -0700

    referencing the bookService and hook it up to bookview

--- FirstWebDevelopment/src/services/goodreadsService.js
+++ FirstWebDevelopment/src/services/goodreadsService.js
@@ -4,6 +4,9 @@ var goodreadsService = function () {
     var getBookById = function (id, callback) {
         callback(null, { description: "Our Description" });
     };
+    return {
+        getBookById: getBookById
+    }
 };
 
 
