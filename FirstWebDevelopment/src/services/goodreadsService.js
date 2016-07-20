5565e663749a1a0a8bba379bc7f2bf155a92f4e6
Fay-OK <Farzaneh.orak@gmail.com>
2016-07-19 20:28:04 -0700

    continue work on getting back data and parsing

--- FirstWebDevelopment/src/services/goodreadsService.js
+++ FirstWebDevelopment/src/services/goodreadsService.js
@@ -1,8 +1,29 @@
 
+var http = require('http');
+var xml2js = require('xml2js');
+var parser = xml2js.parser({ explicitArray: false });
 
 var goodreadsService = function () {
-    var getBookById = function (id, callback) {
-        callback(null, { description: "Our Description" });
+    var getBookById = function (id, cb) {
+        var options = {
+            host: 'www.goodreads.com',
+            path: 'https://www.goodreads.com/book/show/656.xml?key=Z8JHq069t9PjStyw2rKTw'
+        };
+
+        var callback = function (response) {
+            var str = '';
+
+            response.on('data', function (chunck) {
+                str += chunck;
+            });
+            response.on('end', function () {
+                console.log(str);
+                parser.parseString(str, function (err, result) {
+                    cb(null, result.GoodreadsResponse.book);
+                });
+            });
+        }
+        http.request(options, callback).end();
     };
     return {
         getBookById: getBookById
